b9d303496cf90abd9f27e2e2c965a77b856e2944
NDHunq <22520514@gm.uit.edu.vn>
2026-03-24 16:21:08 +0700

    feat: add hero header UI view

---
+++ NetflixClone/Views/HeroHeaderUIView.swift
@@ -0,0 +1,45 @@
+//
+//  HeroHeaderUIView.swift
+//  NetflixClone
+//
+//  Created by Nguyen Duy Hung on 24/3/26.
+//
+
+import UIKit
+
+class HeroHeaderUIView: UIView {
+    
+    private let heroImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFill
+        imageView.clipsToBounds = true
+        imageView.image = UIImage(named: "dune")
+        return imageView
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        addSubview(heroImageView)
+        addGradient()
+    }
+    
+    private func addGradient() {
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.colors = [
+            UIColor.clear.cgColor,
+            UIColor.systemBackground.cgColor
+        ]
+        gradientLayer.frame = bounds
+        layer.addSublayer(gradientLayer)
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        heroImageView.frame = bounds
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError()
+    }
+
+}
